f46ffc51e8f5ea4d7551d53fb63a30add7123a08
roabrin <javiermarintrujillo@gmail.com>
2024-04-18 11:28:02 +0200

    Tercer Commit

--- Fibonacci_entorno/src/Fibona.java
+++ Fibonacci_entorno/src/Fibona.java
@@ -33,5 +33,17 @@ public class Fibona {
 	        }
 			return factor;
 		}
+		public boolean esPrimo(int numero) {
+		    if (numero <= 1) {
+		        return false;
+		    }
+		    for (int i = 2; i < numero; i++) {
+		        if (numero % i == 0) {
+		            return false;
+		        }
+		    }
+		    return true;
+		}
+
 	}
 }
